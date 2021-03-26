ad8765a186f3c5036820c3c4e7860f7663df688a
julian Henao <julianhm9612@gmail.com>
2021-03-26 01:10:39 -0500

    Clean code, add new styles, add and fix animations

--- app/scripts/components/header.js
+++ app/scripts/components/header.js
@@ -23,5 +23,29 @@ angular.module('angularjsLandingApp')
 
       // $scope.$on('', function(event, args) {
       // });
+
+      $scope.scroll = function(route) {
+        route = route === '' ? window.location.pathname : route;
+        var myNav = document.getElementById('app-header');
+        if (route === '/') {
+          myNav.classList.remove('header-no-padding');
+          window.onscroll = function () {
+            if (document.body.scrollTop >= 400 || document.documentElement.scrollTop >= 400) {
+                myNav.classList.add('header-no-padding');
+            } else {
+                myNav.classList.remove('header-no-padding');
+            }
+          };
+        } else {
+          window.onscroll = null;
+          myNav.classList.add('header-no-padding');
+        }
+      };
+
+      $scope.$on('$routeChangeStart', function($event, next, current) {
+        $scope.scroll(next.$$route.originalPath);
+      });
+
+      $scope.scroll('');
     }
   });
